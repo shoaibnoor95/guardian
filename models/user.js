554af75f0570205416608fdb9d6a6697fdc43611
shoaib <shoaib@startrum.com>
2020-08-24 00:54:02 +0500

    file changes

--- models/user.js
+++ models/user.js
@@ -2,41 +2,165 @@ var bcrypt = require("bcrypt-nodejs");
 var mongoose = require("mongoose");
 
 var SALT_FACTOR = 10;
-
 var userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  displayName: String,
-  bio: String
+  firstName: { type: String },
+  lastName: { type: String },
+  phoneNo: { type: String },
+  address: { type: String },
+  differentMailingAddress: { type: Boolean, default: false },
+  streetAddress: { type: String },
+  country: { type: String },
+  province: { type: String },
+  mailingZipcode: String,
+  mailingProvince: { type: String },
+  addressemploye: String,
+  zipcode: String,
+  trusted_person: Boolean,
+  trustedPersonName: String,
+  occupation: String,
+  tele_num: { type: String },
+  city: { type: String },
+  yearsemploye: { type: String },
+  employee_states: { type: String },
+  employee_city: { type: String },
+  employee_zip: { type: String },
+  employee_aptsuite: { type: String },
+  date: { type: Date },
+  employeerphoneNumber: { type: String },
+  relationshipAccountHoler: { type: String },
+  registerationType: { type: String },
+  productToTrade: { type: String },
+  referer: { type: String },
+  employee_country: { type: String },
+  martial_status: { type: String },
+  employee_phone: { type: String },
+  depandents: { type: Number },
+  trustedPersonPhone: String,
+  trustedPersonStreet: String,
+  trustedPersonZip: String,
+  trustedPersonRelation: String,
+  employee: { type: String },
+  emplyername: { type: String },
+  bussinessname: { type: String },
+  companyType: { type: String },
+  yearsofbusiness: { type: Number },
+  bussinessPHONE: { type: Number },
+  trustedPersonState: String,
+  trustedPersonStreet: { type: String },
+  fac: { type: Number },
+  suitNo: { type: String },
+  date_of_birth: { type: Date },
+  id_type: { type: String },
+  exp_date: { type: Date },
+  mailingCountry: { type: String },
+  issue_date: { type: Date },
+  name_of_id: { type: String },
+  anual_income: { type: String },
+  net_worth: { type: String },
+  licenseExp: Date,
+  liquid_net_worth: { type: String },
+  funding: { type: String },
+  nAMEOFBANK: { type: String },
+  accountNo: { type: String },
+  baSwift: { type: String },
+  taxStatus: { type: String },
+  financialgoal: { type: String },
+  investment: { type: String },
+  retir_saving: { type: Boolean },
+  indi_account: { type: Boolean },
+  tax_rate: { type: String },
+  tax_residance: { type: String },
+  inheritnce: Boolean,
+  secu_bnefit: Boolean,
+  bank_funding: String,
+  broker_account: String,
+  affi_zip: String,
+  optradio: String,
+  affi_entity_name: String,
+  affi_adre: String,
+  affi_city: String,
+  affi_state: String,
+  option_check: String,
+  future_check: String,
+  bond_check: String,
+  senior_politician: String,
+  beneficial: String,
+  shareholder: String,
+  household: String,
+  securities: String,
+  under_us_2: String,
+  tax_witholding_2: String,
+  compny_name: String,
+  municipal: String,
+  under_us: String,
+  tax_witholding: String,
+  institution: String,
+  immediate: String,
+  stock_check: String,
+  bond_opt: String,
+  options_opt: String,
+  future_opt: String,
+  account_risk: String,
+  routing: String,
+  acc_num: String,
+  Stocks: Boolean,
+  Bond: Boolean,
+  options: Boolean,
+  Future: Boolean,
+  stock_opt: String,
+  agrem: String,
+  liquidity_exp: String,
+  special_exp: String,
+  expenses: String,
+  other_input: Boolean,
+  another_acc: Boolean,
+  place_country: String,
+  business_prop: Boolean,
+  knowledge: { type: String },
+  brokerage: { type: Boolean },
+  accountatVelocityClearingLLC: { type: Boolean },
+  securitiesexchange: { type: Boolean },
+  financial: { type: Boolean },
+  withholding: { type: Boolean },
+  relativeseniorpoliticalfigure: { type: Boolean },
+  sec: { type: Boolean },
+  companyName: { type: String },
+  symbolCUSIP: { type: String },
+  initialbalance: { type: String },
 });
 
-var noop = function() {};
+var noop = function () {};
 
-userSchema.pre("save", function(done) {
+userSchema.pre("save", function (done) {
   var user = this;
 
   if (!user.isModified("password")) {
     return done();
   }
 
-  bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
-    if (err) { return done(err); }
-    bcrypt.hash(user.password, salt, noop, function(err, hashedPassword) {
-      if (err) { return done(err); }
+  bcrypt.genSalt(SALT_FACTOR, function (err, salt) {
+    if (err) {
+      return done(err);
+    }
+    bcrypt.hash(user.password, salt, noop, function (err, hashedPassword) {
+      if (err) {
+        return done(err);
+      }
       user.password = hashedPassword;
       done();
     });
   });
 });
 
-userSchema.methods.checkPassword = function(guess, done) {
-  bcrypt.compare(guess, this.password, function(err, isMatch) {
+userSchema.methods.checkPassword = function (guess, done) {
+  bcrypt.compare(guess, this.password, function (err, isMatch) {
     done(err, isMatch);
   });
 };
 
-userSchema.methods.name = function() {
+userSchema.methods.name = function () {
   return this.displayName || this.username;
 };
 
--- routes.js
+++ routes.js
@@ -13,283 +13,174 @@ function ensureAuthenticated(req, res, next) {
   }
 }
 
-router.use(function(req, res, next) {
+router.use(function (req, res, next) {
   res.locals.currentUser = req.user;
   res.locals.errors = req.flash("error");
   res.locals.infos = req.flash("info");
   next();
 });
-
-
-router.get("/", function(req, res) {
+router.post("/form", ensureAuthenticated, function (req, res, next) {
+  req.user.firstName = req.body.firstName;
+  req.user.middleName = req.body.middleName;
+  req.user.lastName = req.body.lastName;
+  req.user.suitNo = req.body.suitNo;
+  req.user.address = req.body.address;
+  req.user.streetAddress = req.body.streetAddress;
+  req.user.country = req.body.country;
+  req.user.province = req.body.province;
+  req.user.city = req.body.city;
+  req.user.occupation = req.body.occupation;
+  req.user.differentMailingAddress = req.body.differentMailingAddress;
+  req.user.martial_status = req.body.martial_status;
+  req.user.tele_num = req.body.tele_num;
+  req.user.depandents = req.body.depandents;
+  req.user.trusted_person = req.body.trusted_person;
+  req.user.trustedPersonPhone = req.body.trustedPersonPhone;
+  req.user.trustedPersonState = req.body.trustedPersonState;
+  req.user.trustedPersonCountry = req.body.trustedPersonCountry;
+  req.user.trustedPersonCity = req.body.trustedPersonCity;
+  req.user.trustedPersonStreet = req.body.trustedPersonStreet;
+  req.user.trustedPersonZip = req.body.trustedPersonZip;
+  req.user.trustedPersonRelation = req.body.trustedPersonRelation;
+  req.user.employee = req.body.employee;
+  req.user.exp_date = req.body.exp_date;
+  req.user.zipcode = req.body.zipcode;
+  req.user.emplyername = req.body.emplyername;
+  req.user.yearsemploye = req.body.yearsemploye;
+  req.user.employee_country = req.body.employee_country;
+  req.user.employee_states = req.body.employee_states;
+  req.user.employee_zip = req.body.employee_zip;
+  req.user.trustedPersonEmail = req.body.trustedPersonEmail;
+  req.user.employee_city = req.body.employee_city;
+  req.user.employee_phone = req.body.employee_phone;
+  req.user.trustedPersonStreet = req.body.trustedPersonStreet;
+  req.user.employee_fax = req.body.employee_fax;
+  req.user.id_type = req.body.id_type;
+  req.user.issue_date = req.body.issue_date;
+  req.user.date_of_birth = req.body.date_of_birth;
+  req.user.employee_aptsuite = req.body.employee_aptsuite;
+  req.user.name_of_id = req.body.name_of_id;
+  req.user.licenseExp = req.body.licenseExp;
+  req.user.place_country = req.body.place_country;
+  req.user.employeerphoneNumber = req.body.employeerphoneNumber;
+  req.user.employeerFax = req.body.employeerFax;
+  req.user.mailingCountry = req.body.mailingCountry;
+  req.user.mailingProvince = req.body.mailingProvince;
+  req.user.mailingZipcode = req.body.mailingZipcode;
+  req.user.tax_residance = req.body.tax_residance;
+  req.user.anual_income = req.body.anual_income;
+  req.user.net_worth = req.body.net_worth;
+  req.user.liquid_net_worth = req.body.liquid_net_worth;
+  req.user.tax_rate = req.body.tax_rate;
+  req.user.retir_saving = req.body.retir_saving;
+  req.user.indi_account = req.body.indi_account;
+  req.user.another_acc = req.body.another_acc;
+  req.user.gift = req.body.gift;
+  req.user.business_prop = req.body.business_prop;
+  req.user.inheritnce = req.body.inheritnce;
+  req.user.secu_bnefit = req.body.secu_bnefit;
+  req.user.other_input = req.body.other_input;
+  req.user.bank_funding = req.body.bank_funding;
+  req.user.routing = req.body.routing;
+  req.user.acc_num = req.body.acc_num;
+  req.user.account_risk = req.body.account_risk;
+  req.user.expenses = req.body.expenses;
+  req.user.special_exp = req.body.special_exp;
+  req.user.liquidity_exp = req.body.liquidity_exp;
+  req.user.financialgoal = req.body.financialgoal;
+  req.user.Stocks = req.body.Stocks;
+  req.user.Option = req.body.Option;
+  req.user.Bond = req.body.Bond;
+  req.user.Future = req.body.Future;
+  req.user.stock_opt = req.body.stock_opt;
+  req.user.future_opt = req.body.future_opt;
+  req.user.bond_opt = req.body.bond_opt;
+  req.user.options_opt = req.body.future_opt;
+  req.user.stock_check = req.body.stock_check;
+  req.user.bond_check = req.body.bond_check;
+  req.user.option_check = req.body.option_check;
+  req.user.future_check = req.body.future_check;
+  req.user.broker_account = req.body.broker_account;
+  req.user.beneficial = req.body.beneficial;
+  req.user.shareholder = req.body.shareholder;
+  req.user.immediate = req.body.immediate;
+  req.user.securities = req.body.securities;
+  req.user.institution = req.body.institution;
+  req.user.tax_witholding = req.body.tax_witholding;
+  req.user.under_us = req.body.under_us;
+  req.user.under_us_2 = req.body.under_us_2;
+  req.user.tax_witholding_2 = req.body.tax_witholding_2;
+  req.user.senior_politician = req.body.senior_politician;
+  req.user.household = req.body.household;
+  req.user.municipal = req.body.municipal;
+  req.user.compny_name = req.body.compny_name;
+  req.user.affi_adre = req.body.affi_adre;
+  req.user.affi_entity_name = req.body.affi_entity_name;
+  req.user.affi_city = req.body.affi_city;
+  req.user.affi_state = req.body.affi_state;
+  req.user.affi_zip = req.body.affi_zip;
+  req.user.agrem = req.body.agrem;
+  req.user.save();
+  req.flash("info", "User has been successfully saved");
+  res.redirect("/login");
+});
+router.get("/", function (req, res, next) {
   res.render("login");
 });
 
-router.post("/login", passport.authenticate("login", {
-  successRedirect: "/",
-  failureRedirect: "/",
-  failureFlash: true
-}));
-
-router.get("/logout", function(req, res) {
+router.post(
+  "/login",
+  passport.authenticate("login", {
+    successRedirect: "/",
+    failureRedirect: "/",
+    failureFlash: true,
+  })
+);
+router.get("/form", ensureAuthenticated, function (req, res) {
+  res.render("form");
+});
+router.get("/logout", function (req, res) {
   req.logout();
   res.redirect("/");
 });
 
-router.get("/register", function(req, res) {
+router.get("/signup", function (req, res) {
   res.render("signup");
 });
 
-router.get("/form",ensureAuthenticated, function(req, res) {
-  res.render("form");
-});
-
-router.post('/form',ensureAuthenticated,(req,res,next)=>{
-  req.user.firstName=req.body.firstName
-  req.user.country=req.body.country
-
-  req.user.middleName=req.body.middleName
-  req.user.lastName=req.body.lastName
-  req.user.address=req.body.address
-  req.user.apt=req.body.apt
-  req.user.state=req.body.state
-  req.user.city=req.body.city
-  req.user.zipCode=req.body.zipCode
-  req.user.diffentMailingAddress=req.body.diffentMailingAddress
-
-  req.user.mailingAddress=req.body.mailingAddress
-  req.user.mailingApt=req.body.mailingApt
-  req.user.mailingCountry=req.body.mailingCountry
-  req.user.mailingState=req.body.mailingState
-  req.user.mailingCity=req.body.mailingCity
-  req.user.mailingZipCode=req.body.mailingZipCode
-  req.user.martialStatus=req.body.martialStatus
-  
-  req.user.telephoneNumber=req.body.telephoneNumber
-
-  
-  req.user.numberDependents=req.body.numberDependents
-  req.user.trustedPerson=req.body.trustedPerson
-  req.user.trustedName=req.body.trustedName
-  req.user.trustedPhonenumber=req.body.trustedPhonenumber
-  req.user.trustedEmail=req.body.trustedEmail
-
-  req.user.trustedAddress=req.body.trustedAddress
-  
-  req.user.trustedState=req.body.trustedState
-
-  req.user.trustedCountry=req.body.trustedCountry
-  req.user.trustedCity=req.body.trustedCity
-
-  req.user.trustedZipCode=req.body.trustedZipCode
-
-  req.user.trustedRelation=req.body.trustedRelation
-  
-//2NDF
-req.user.areYouCurrently=req.body.areYouCurrently
-
-req.user.employeeName=req.body.employeeName
-
-req.user.occupation=req.body.occupation
-
-req.user.addressOfEmployee=req.body.addressOfEmployee
-
-req.user.yersOfEmployee=req.body.yersOfEmployee
-req.user.employeeState=req.body.employeeState
-req.user.employeeZipCode=req.body.employeeZipCode
-req.user.employeeCity=req.body.employeeCity
-
-req.user.employeePhoneNumber=req.body.employeePhoneNumber
-
-
-req.user.employeeApt=req.body.employeeApt
-
-
-req.user.employeeFax=req.body.employeeFax
-
-
-
-
-req.user.employeeCountry=req.body.employeeCountry
-
-
-
-
-
-//3f
-req.user.customerDatebirth=req.body.customerDatebirth
-
-req.user.customerIdtype=req.body.customerIdtype
-req.user.customerIdnumber=req.body.customerIdnumber
-
-req.user.customerExpiration=req.body.customerExpiration
-req.user.customerPlace=req.body.customerPlace
-
-req.user.customerIssueDate=req.body.customerIssueDate
-
-req.user.customerTaxResidence=req.body.customerTaxResidence
-
- //4F
- 
-
- req.user.annualIncome=req.body.annualIncome
- req.user.netWorth=req.body.netWorth
-
-
- req.user.liquidNet=req.body.liquidNet
-
- req.user.tax=req.body.tax
-
-
- //5F
- req.user.income=req.body.income
- req.user.pension=req.body.pension
-
- 
- req.user.funds=req.body.funds
- req.user.gift=req.body.gift
-
-
- req.user.inheritance=req.body.inheritance
-
-  req.user.other=req.body.other
-  req.user.others=req.body.others
-
- req.user.social=req.body.social
-
- 
- req.user.nameofTheBank=req.body.nameofTheBank 
- req.user.abaSwift=req.body.abaSwift
- req.user.accountNumber=req.body.accountNumber
-
-
- 
-
- //6f
-
-
- 
- req.user.riskAcceptance=req.body.riskAcceptance
- req.user.annualExpence=req.body.annualExpence
-
-
- req.user.specialExpence=req.body.specialExpence
- req.user.liquidityNeeds=req.body.liquidityNeeds
- expectedPeriod
- req.user.expectedPeriod=req.body.expectedPeriod
-
- 
- //7F
- req.user.Stocks=req.body.Stocks
-
- req.user.Stocks=req.body.Bond
- req.user.Options=req.body.Options
- req.user.Future=req.body.Future
- req.user.stock_opt=req.body.stock_opt
-
- req.user. bond_opt =req.body. bond_opt
-
- 
- req.user. options_opt =req.body. options_opt
- 
- 
- req.user. future_opt =req.body. future_opt
- 
- req.user. stock_check =req.body. stock_check
- 
- req.user. bond_check =req.body. bond_check
-
- 
- req.user. option_check =req.body. option_check
-
- req.user. broker_account =req.body. broker_account
- req.user. future_check =req.body. future_check
-
- req.user. beneficial =req.body. beneficial
-
- req.user. finra =req.body. finra
-
- req.user. institution =req.body. institution
- req.user. under_us =req.body. under_us
- 
- req.user. tax_witholding =req.body. tax_witholding
-
-
-
- req.user. seniorOfficer =req.body. seniorOfficer
-
-
- 
-
- 
-//F8
-req.user. senior =req.body. senior
-req.user. household=req.body. household
-req.user. disclosuresCompanyname=req.body. disclosuresCompanyname
-
-municipal
-req.user. municipal =req.body. municipal
-req.user. symbol =req.body. symbol
-req.user. disclosureCompanyname =req.body. disclosureCompanyname
-req.user. disclosureAddress =req.body. disclosureAddress
-
-req.user. disclosureCity =req.body. disclosureCity
-
-
-req.user. disclosureState =req.body. disclosureState
-
-req.user. disclosureZipCode =req.body. disclosureZipCode
-
-req.user. disclosureclient =req.body. disclosureclient
-
-req.user. agrem =req.body. agrem
-
-
-
-req.user. penny =req.body. penny
-
-req.user. online_risj =req.body. online_risj
-
-req.user. anti_money =req.body. anti_money
-
-req.user. margin_disc =req.body. margin_disc
-
-req.user. petren =req.body. petren
-
-
-
-req.user. confirmed_electronic =req.body. confirmed_electronic
-
-req.flash("info", "Form Submitted!");
-res.redirect("/");
-
-})
-
-router.post("/signup", function(req, res, next) {
-
-  var email = req.body.email;
-  var password = req.body.password;
-
-  User.findOne({ email: email }, function(err, user) {
-
-    if (err) { return next(err); }
-    if (user) {
-      req.flash("error", "User already exists");
-      return res.redirect("/register");
-    }
-
-    var newUser = new User({
-      email: email,
-      password: password
+router.post(
+  "/signup",
+  function (req, res, next) {
+    const email = req.body.email;
+    const password = req.body.password;
+    const registerationType = req.body.RegisterationType;
+    const productToTrade = req.body.ProductToTrade;
+    const referer = req.body.referer;
+
+    User.findOne({ email: email }, function (err, user) {
+      if (err) {
+        return next(err);
+      }
+      if (user) {
+        req.flash("error", "User already exists");
+        return res.redirect("/signup");
+      }
+
+      var newUser = new User({
+        email: email,
+        password: password,
+        registerationType,
+        productToTrade,
+        referer,
+      });
+      newUser.save(next);
     });
-    newUser.save(next);
-
-  });
-}, passport.authenticate("login", {
-  successRedirect: "/form",
-  failureRedirect: "/register",
-  failureFlash: true
-}));
-
+  },
+  passport.authenticate("login", {
+    successRedirect: "/form",
+    failureRedirect: "/signup",
+    failureFlash: true,
+  })
+);
 
 module.exports = router;
